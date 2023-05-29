2679cdbbf358d93adaef8e9ac7b7f439493dfbff
marcospm99 <mpiedrabuenam99@gmail.com>
2023-05-29 10:11:30 +0200

    interPipe radial dir ready

--- source/program/parameters.f90
+++ source/program/parameters.f90
@@ -30,13 +30,13 @@
    implicit none
    save
 
-   integer,          parameter :: i_N           = 384 ! n in [1,N] radial r_n
+   integer,          parameter :: i_N           = 192 ! n in [1,N] radial r_n
    integer,          parameter :: i_K           = 512 ! ! Este tiene que ser potencia de 2 y 3 y grande!!
-   integer,          parameter :: i_M           = 128 ! azimutal 
+   integer,          parameter :: i_M           = 256 ! azimutal 
    integer,          parameter :: i_Mp          = 1  ! Siempre 1. 
 
 !-- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- --
-   double precision            :: d_Re          = 5300d0
+   double precision            :: d_Re          = 19000d0
    double precision            :: d_alpha       = 0.2d0
    logical,          parameter :: b_const_flux  = .true.
    logical,          parameter :: b_mirrorsym   = .false.
@@ -45,14 +45,14 @@
    double precision, parameter :: d_minE3d      = 1d-5
    
 !-- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- --
-   integer,          parameter :: i_save_rate1  = 30
-   integer,          parameter :: i_save_rate2  = 30
+   integer,          parameter :: i_save_rate1  = 1000
+   integer,          parameter :: i_save_rate2  = 1000
    integer,          parameter :: i_maxtstep    = -1 
    integer,          parameter :: s_step        = 2 ! interval to take statistics. 
    integer,          parameter :: f_step        = 1d8 ! Steps
    double precision, parameter :: d_maxt        = -1d0
    double precision, parameter :: d_cpuhours    = 1d99 !90d0
-   double precision, parameter :: d_time        = -1d0 !-1d0 !0d0
+   double precision, parameter :: d_time        = 0d0 !-1d0 !0d0   !!!!!!!!!
    double precision, parameter :: d_timestep    = -1d0 !0.01d0 !-1d0
    double precision, parameter :: d_maxdt       = 1d99
    double precision, parameter :: d_dterr       = 1d-5 !1d99
